1ea9524c6623f116b4fa0f2bf3d7f0a491308809
mohamed <mohamedebra>
2024-04-28 09:11:39 +0300

    upload

--- lib/views/widgets/custom_textfield.widgets.dart
+++ lib/views/widgets/custom_textfield.widgets.dart
@@ -88,6 +88,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
               hintStyle: TextStyle(
                   fontWeight: FontWeight.w600,
                   fontSize: 15.sp,
+
                   color: AppColors.blackColor),
               hintText: widget.hint,
               isDense: true,
